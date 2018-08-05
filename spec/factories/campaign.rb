765341a9c52f953ec95bc0e8922295377d26d628
Mateus Braga <mateuscbraga@gmail.com>
2018-08-05 12:56:56 -0300

    added some changes

--- spec/factories/campaign.rb
+++ spec/factories/campaign.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
-    factory :campaign do
-      title         { Faker::Lorem.word }
-      description   { Faker::Lorem.sentence }
-      user
-    end
-end
+  factory :campaign do
+    title         { Faker::Lorem.word }
+    description   { Faker::Lorem.sentence }
+    user
+    status        { :pending }
+    locale        { "#{Faker::Address.city}, #{Faker::Address.street_address}"}
+    event_date    { Faker::Time.between(2.days.ago, Date.today, :all) }
+    event_hour    { rand(24).to_s }
+  end
+ end
